fa4dc90311b4e9f2e1339e09e0a439018681f9a8
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2024-03-08 21:24:38 -0300

    Polimorfismo Sobrecarga

---
+++ src/aula13/Aula13.java
@@ -0,0 +1,12 @@
+package aula13;
+
+public class Aula13 {
+    public static void main(String[] args) {
+        Cachorro c = new Cachorro();
+        c.reagir(true);
+        c.reagir(false);
+        c.reagir(11, 45);
+        c.reagir(19, 00);
+
+    }
+}
